f55fb55ce93f72ded9904f2bb965ec561b7d9674
esteinig <eike.steinig@unimelb.edu.au>
2024-09-24 08:30:25 +1000

    chore(dev): add lib prelude

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,10 @@ pub mod mknn;
 pub mod terminal;
 pub mod log;
 pub mod netview;
+
+pub mod prelude {
+    pub use crate::netview::*;
+    pub use crate::dist::*;
+    pub use crate::mknn::*;
+    pub use crate::error::*;
+}
